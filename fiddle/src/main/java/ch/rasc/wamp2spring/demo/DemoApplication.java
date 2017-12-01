472de5a8ba04e5c85bb711dce334143c0f663487
Ralph Schaer <ralphschaer@gmail.com>
2017-12-01 12:47:29 +0100

    Upgrade libraries

--- fiddle/src/main/java/ch/rasc/wamp2spring/demo/DemoApplication.java
+++ fiddle/src/main/java/ch/rasc/wamp2spring/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.socket.server.HandshakeHandler;
 
+import ch.rasc.wamp2spring.config.Feature;
+import ch.rasc.wamp2spring.config.Features;
 import ch.rasc.wamp2spring.config.PreferJsonHandshakeHandler;
 import ch.rasc.wamp2spring.config.WampConfiguration;
 
@@ -14,12 +16,12 @@ public class DemoApplication extends WampConfiguration {
 	public static void main(String[] args) {		
 		SpringApplication.run(DemoApplication.class, args);
 	}
-//
-//	@Override
-//	protected void configureFeatures(Features features) {
-//		features.disable(Feature.DEALER);
-//	}
-//
+
+	@Override
+	protected void configureFeatures(Features features) {
+		features.disable(Feature.DEALER);
+	}
+
 	@Override
 	protected HandshakeHandler getHandshakeHandler() {
 		return new PreferJsonHandshakeHandler();
--- smoothie/src/main/java/ch/rasc/wamp2spring/smoothie/Application.java
+++ smoothie/src/main/java/ch/rasc/wamp2spring/smoothie/Application.java
@@ -3,16 +3,26 @@ package ch.rasc.wamp2spring.smoothie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.server.HandshakeHandler;
 
 import ch.rasc.wamp2spring.config.EnableWamp;
 import ch.rasc.wamp2spring.config.Feature;
+import ch.rasc.wamp2spring.config.PreferJsonHandshakeHandler;
+import ch.rasc.wamp2spring.config.WampConfiguration;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableWamp(disable = Feature.DEALER)
-public class Application {
+public class Application extends WampConfiguration {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+	protected HandshakeHandler getHandshakeHandler() {
+		return new PreferJsonHandshakeHandler();
+		// return new PreferCborHandshakeHandler();
+	}
+
 }
