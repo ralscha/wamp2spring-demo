38da3b0103b96ff994ebc92d6e1a7a7f15f88b66
Ralph Schaer <ralphschaer@gmail.com>
2021-01-10 13:29:00 +0100

    Fix mongodb dependency

--- session/src/main/java/ch/rasc/wamp2spring/demo/SessionConfiguration.java
+++ session/src/main/java/ch/rasc/wamp2spring/demo/SessionConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClients;
 
 import ch.rasc.wamp2spring.session.servlet.EnableWampServletSession;
 
@@ -16,7 +16,7 @@ public class SessionConfiguration {
 	@SuppressWarnings("resource")
 	@Bean
 	public MongoOperations mongoOperations() {
-		return new MongoTemplate(new MongoClient(), "sessiondemo");
+		return new MongoTemplate(MongoClients.create(), "sessiondemo");
 	}
 
 }
